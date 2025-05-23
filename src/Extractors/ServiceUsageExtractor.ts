42906357de417e484a946e78e3c083ebf613b8d5
Alex Nair <Alex.Nair@dell.com>
2025-05-23 09:23:58 -0600

    adding unused service usuage

--- src/Extractors/ServiceUsageExtractor.ts
+++ src/Extractors/ServiceUsageExtractor.ts
@@ -1,4 +1,4 @@
-import { Project, SourceFile, SyntaxKind } from "ts-morph";
+import { Project } from "ts-morph";
 import { Component } from "../Models/Component";
 import { FileHelper } from "../FileHelper";
 import * as path from 'path';
@@ -8,6 +8,7 @@ export class ServiceUsageExtractor {
     services = new Map<string, Component>();
     rootPath: string;
     project: Project;
+
     constructor(rootPath: string, project: Project, services: Map<string, Component>) {
         this.rootPath = rootPath;
         this.project = project;
@@ -15,51 +16,57 @@ export class ServiceUsageExtractor {
     }
 
     public async findUsedServices(): Promise<Map<string, Component>> {
-        let content = '';
-        let lines: string[] = [];
+        const usedServices = new Set<string>();
         const files = this.project.getSourceFiles();
-            for (let i = 0; i < files.length; i++) {
-                const file = files[i];
-                const fileName = path.basename(file.getFilePath());
-                if (!fileName.includes('module')) {
 
-                    const classes = file.getClasses();
-                    classes.forEach(async(classDec) => {
-                        classDec.getConstructors().forEach(async(constructor) => {
-                            constructor.getParameters().forEach(async(param) => {
-                                const paramType = param.getType().getText();
-                                if (this.services.has(paramType)) {
-                                    
-                                    const references = param.findReferencesAsNodes();
-                                    for (const reference of references) {
-                                        const alias = param.getName();
-                                        const service = this.services.get(paramType);
-                                        if (service) {
-                                            if (content.length === 0 && lines.length === 0) {
-                                                content = await FileHelper.GetFileContent(file.getFilePath());
-                                                lines = content.split('\n');
-                                            }
-                                            service.uses.push(new Component(
-                                                '',
-                                                alias,
-                                                TreeItemCollapsibleState.None,
-                                                file.getFilePath(),
-                                                fileName,
-                                                lines[reference.getStartLineNumber() - 1],
-                                                reference.getStartLineNumber().toString(),
-                                                []
-                                            ));
-                                        }
-                                    }
+        for (const file of files) {
+            const fileName = path.basename(file.getFilePath());
+            if (fileName.includes('module')) continue;
 
+            const classes = file.getClasses();
+            for (const classDec of classes) {
+                const constructors = classDec.getConstructors();
+                for (const constructor of constructors) {
+                    const parameters = constructor.getParameters();
+                    for (const param of parameters) {
+                        const paramType = param.getType().getText();
+                        if (this.services.has(paramType)) {
+                            usedServices.add(paramType);
+
+                            const references = param.findReferencesAsNodes();
+                            if (references.length > 0) {
+                                const service = this.services.get(paramType);
+                                const alias = param.getName();
+                                const content = await FileHelper.GetFileContent(file.getFilePath());
+                                const lines = content.split('\n');
+
+                                for (const reference of references) {
+                                    service?.uses.push(new Component(
+                                        '',
+                                        alias,
+                                        TreeItemCollapsibleState.None,
+                                        file.getFilePath(),
+                                        fileName,
+                                        lines[reference.getStartLineNumber() - 1],
+                                        reference.getStartLineNumber().toString(),
+                                        []
+                                    ));
                                 }
-                            });
-                        });
-                    });
+                            }
+                        }
+                    }
                 }
-                content = '';
-                lines = [];
             }
-        return this.services;
+        }
+
+        // Identify unused services
+        const unusedServices = new Map<string, Component>();
+        for (const [serviceName, service] of this.services.entries()) {
+            if (!usedServices.has(serviceName)) {
+                unusedServices.set(serviceName, service);
+            }
+        }
+
+        return unusedServices;
     }
 }
